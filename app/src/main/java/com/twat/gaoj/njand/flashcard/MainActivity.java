ff7892d84ffd611ffbbc590d29a9ab0c871e8d88
andersonn <njanderson2013@gmail.com>
2017-04-03 11:04:09 -0500

    fixed interface

--- app/src/main/java/com/twat/gaoj/njand/flashcard/MainActivity.java
+++ app/src/main/java/com/twat/gaoj/njand/flashcard/MainActivity.java
@@ -3,14 +3,18 @@ package com.twat.gaoj.njand.flashcard;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.twat.gaoj.njand.flashcard.Database.DbAccess;
 import com.twat.gaoj.njand.flashcard.Database.MyDbAccess;
 
+
 public class MainActivity extends AppCompatActivity {
-    MyDbAccess mMyDbAccess;
+    private DbAccess mDbAccess;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mMyDbAccess = new MyDbAccess(getApplicationContext());
+        mDbAccess = new MyDbAccess(getApplicationContext());
+
     }
 }
